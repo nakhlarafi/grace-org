55a16f2c79b37de08683112270edc00d6161467d
Nakhla Rafi <nakhla054@gmail.com>
2023-08-31 15:04:33 -0400

    change in dim

--- Code/Default/run.py
+++ Code/Default/run.py
@@ -25,7 +25,7 @@ class dotdict(dict):
 # Gson 898x312
 # Cli 497x293
 # JacksonCore 289x144
-NlLen_map = {"Time":3900, "Math":3000, "Lang":350, "Chart": 2350, "Mockito":1400, "Closure":5000, "Codec":500, "Compress":1000, "Gson":900, "Cli":500, "Jsoup":2000, "Csv":500, "JacksonCore":1000}
+NlLen_map = {"Time":3900, "Math":3000, "Lang":400, "Chart": 2350, "Mockito":1400, "Closure":5000, "Codec":500, "Compress":1000, "Gson":900, "Cli":500, "Jsoup":2000, "Csv":500, "JacksonCore":1000}
 CodeLen_map = {"Time":1000, "Math":1000, "Lang":400, "Chart":5250, "Mockito":300, "Closure":5000, "Codec":500, "Compress":1500, "Gson":350, "Cli":300, "Jsoup":2000, "Csv":500, "JacksonCore":1000}
 args = dotdict({
     'NlLen':NlLen_map[sys.argv[2]],
