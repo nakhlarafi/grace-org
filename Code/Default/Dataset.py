8bf79056a01d3a91109c4ee566e47ef5acf9b7d9
Nakhla Rafi <nakhla054@gmail.com>
2023-08-31 15:12:14 -0400

    change in dataset

--- Code/Default/Dataset.py
+++ Code/Default/Dataset.py
@@ -443,7 +443,21 @@ class SumDataset(data.Dataset):
                 #nlad[a, b] = 1
                 #nlad[b, a] = 1
            
-            for e in x['propagation_edges']:
+            for e in propagation_edges:
+                a = e[0]
+                b = e[1]
+                
+                # Add edge a->b
+                nladrow.append(a)
+                nladcol.append(b)
+                nladval.append(1)
+
+                # Add edge b->a if you want bidirectional connections
+                nladrow.append(b)
+                nladcol.append(a)
+                nladval.append(1)
+
+            '''for e in x['edge3']:
                 a = e[0] + self.Nl_Len#len(x['ftest']) + methodnum
                 b = e[1] + self.Nl_Len#len(x['ftest']) + methodnum
                 if a == b:
@@ -467,7 +481,7 @@ class SumDataset(data.Dataset):
                 nladval.append(1)
                 nladrow.append(b)
                 nladcol.append(a)
-                nladval.append(1)
+                nladval.append(1)'''
             '''for e in x['edge4']:
                 #assert(0)
                 a = e[0] + len(x['ftest']) + methodnum 
