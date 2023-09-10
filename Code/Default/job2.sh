3d14d3b63c21dfe4bca7c57a9d0d888faa2f7649
Nakhla Rafi <nakhla054@gmail.com>
2023-09-10 19:27:07 -0400

    change in job

--- Code/Default/job2.sh
+++ Code/Default/job2.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-#SBATCH -J grace_test_jack # job name
-#SBATCH --account=r_mdnakh # indicates username (mandatory parameter)
-#SBATCH --nodelist=virya3 # specific node to run the job on.
-#SBATCH --mem=100M # memory reserved (mandatory parameter)
-#SBATCH -o _%x%J.out # output (& error) file name
-#SBATCH --mail-type=BEGIN,END # when to send email notification
-#SBATCH --mail-user=nakhla054@gmail.com # set email to be used
+#SBATCH -J grace_test_jack 
+#SBATCH --account=r_mdnakh 
+#SBATCH --nodelist=virya3 
+#SBATCH --mem=100M 
+#SBATCH -o _%x%J.txt
 
 module load anaconda/3.2022.10
 python3 runtotal.py JacksonXml
