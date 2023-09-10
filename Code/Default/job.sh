2311be8fd8b8c617dab669b1f641f40cc4b8c4a2
Nakhla Rafi <nakhla054@gmail.com>
2023-09-10 19:46:39 -0400

    chnage in job

--- Code/Default/job.sh
+++ Code/Default/job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -J Test-GPUs --mem=100M --account=r_mdnakh --gpus=40gb:1
 nvidia-smi -L
-
+source /etc/profile.d/modules.sh
 module load anaconda/3.2022.10
 python runtotal.py JacksonXml
