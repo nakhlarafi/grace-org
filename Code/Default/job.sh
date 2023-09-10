4971b4bcf24b324254183bb46fe0d8edc95d2ec5
Nakhla Rafi <nakhla054@gmail.com>
2023-09-10 19:45:15 -0400

    change in job

--- Code/Default/job.sh
+++ Code/Default/job.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#SBATCH -J Test-GPUs --mem=120M --account=r_mdnakh --gpus=40gb:1
+#SBATCH -J Test-GPUs --mem=100M --account=r_mdnakh --gpus=40gb:1
+nvidia-smi -L
 
 module load anaconda/3.2022.10
 python runtotal.py JacksonXml
