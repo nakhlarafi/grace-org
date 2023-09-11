204d53b2572eefb60066d660ced94e76a04ef110
Nakhla Rafi <nakhla054@gmail.com>
2023-09-11 10:20:09 -0400

    change in dim

--- Code/Default/job.sh
+++ Code/Default/job.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #SBATCH -J Test-GPUs --mem=100M --account=r_mdnakh --gpus=40gb:1
+#SBATCH --mail-type=BEGIN,END 
+#SBATCH --mail-user=nakhla054@gmail.com 
+
 nvidia-smi -L
 source /etc/profile.d/modules.sh
 module load anaconda/3.2022.10
