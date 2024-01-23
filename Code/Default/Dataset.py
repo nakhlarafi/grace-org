449576b54b146f4eebb971fb09b58836380fbd66
Nakhla Rafi <nakhla054@gmail.com>
2024-01-22 22:20:57 -0500

    changes

--- Code/Default/Dataset.py
+++ Code/Default/Dataset.py
@@ -445,19 +445,19 @@ class SumDataset(data.Dataset):
                 #nlad[a, b] = 1
                 #nlad[b, a] = 1
            
-            for e in x['propagation_edges']:
-                a = e[0]
-                b = e[1]
+            # for e in x['propagation_edges']:
+            #     a = e[0]
+            #     b = e[1]
                 
-                # Add edge a->b
-                nladrow.append(a)
-                nladcol.append(b)
-                nladval.append(1)
+            #     # Add edge a->b
+            #     nladrow.append(a)
+            #     nladcol.append(b)
+            #     nladval.append(1)
 
-                # Add edge b->a if you want bidirectional connections
-                nladrow.append(b)
-                nladcol.append(a)
-                nladval.append(1)
+            #     # Add edge b->a if you want bidirectional connections
+            #     nladrow.append(b)
+            #     nladcol.append(a)
+            #     nladval.append(1)
 
             # for e in x['edge3']:
             #     a = e[0] + self.Nl_Len#len(x['ftest']) + methodnum
