4bdd237a8fd5bba46e3ac39fe471feffc5842ed3
Nakhla Rafi <nakhla054@gmail.com>
2024-01-24 16:58:29 -0500

    fd

--- Code/Default/merge_results.py
+++ Code/Default/merge_results.py
@@ -3,8 +3,19 @@ import pickle
 import json
 import sys
 
-def read_and_merge_pkl_files(folder_path, output_json_file):
+def read_and_merge_pkl_files(folder_path, output_json_file, additional_pkl_file, project_name):
     merged_data = {}
+
+    # Read additional .pkl file first
+    with open(additional_pkl_file, 'rb') as file:
+        additional_data = pickle.load(file)
+        # Find the matching entry for the project
+        for d in additional_data:
+            if d['proj'] == project_name:
+                additional_info = d['ans']
+                break
+        else:
+            additional_info = None
 
     # Iterate over all files in the given folder
     for file in os.listdir(folder_path):
@@ -19,14 +30,19 @@ def read_and_merge_pkl_files(folder_path, output_json_file):
                 if isinstance(data, tuple) and len(data) == 2:
                     merged_data[data[0]] = data[1]
 
+    # Add additional info if available
+    if additional_info:
+        merged_data['ans'] = additional_info
+
     # Write merged data to a JSON file
     with open(output_json_file, 'w') as json_file:
         json.dump(merged_data, json_file, indent=4)
 
     print(f'Merged data written to {output_json_file}')
 
-project_name = sys.argv[1]
 # Example usage
-folder_path = f'crossvalidation/{project_name}'  # Replace with the path to your folder
-output_json_file = f'{project_name}_merged_data.json'
-read_and_merge_pkl_files(folder_path, output_json_file)
+project_name = sys.argv[1]
+folder_path = f'crossvalidation/{project_name}'  # Folder with .pkl files
+additional_pkl_file = f'{project_name}.pkl'  # Path to the additional .pkl file
+output_json_file = f'crossvalidation/{project_name}_merged_data.json'
+read_and_merge_pkl_files(folder_path, output_json_file, additional_pkl_file, project_name)
