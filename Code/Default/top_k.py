452ea0688192a61d1459a1ed131a8813e2c7342e
Nakhla Rafi <nakhla054@gmail.com>
2024-01-24 17:22:17 -0500

    g

--- Code/Default/top_k.py
+++ Code/Default/top_k.py
@@ -5,50 +5,40 @@ def calculate_metrics(json_file):
     with open(json_file, 'r') as file:
         data = json.load(file)
 
-    top1, top3, top5, top10 = 0, 0, 0, 0
-    mfr_list, mar_list = []
+    top_count = [0] * 5  # Initialize top count for positions 1 to 5
+    mfr_list, mar_list = [], []
 
     for project, info in data.items():
         ranking = info['ranking']
         ground_truth = info['ans']
 
-        min_rank, ranks = float('inf'), []
+        ranks, min_rank = [], float('inf')
 
         for gt in ground_truth:
             rank = ranking.index(gt)
             ranks.append(rank)
             min_rank = min(min_rank, rank)
 
-        # Update counts for top-n metrics
-        if min_rank == 0:
-            top1 += 1
-        if min_rank < 3:
-            top3 += 1
-        if min_rank < 5:
-            top5 += 1
-        if min_rank < 10:
-            top10 += 1
+            # Update top count for this rank
+            if rank < len(top_count):
+                top_count[rank] += 1
 
         mfr_list.append(min_rank)
         mar_list.append(np.mean(ranks))
 
-    num_projects = len(data)
+    top1 = top_count[0]
+    top3 = sum(top_count[:3])
+    top5 = sum(top_count[:5])
     mfr_avg = np.mean(mfr_list)
     mar_avg = np.mean(mar_list)
 
     return {
-        'Top-1': top1 / num_projects,
-        'Top-3': top3 / num_projects,
-        'Top-5': top5 / num_projects,
-        'Top-10': top10 / num_projects,
+        'Top-1': top1,
+        'Top-3': top3,
+        'Top-5': top5,
         'MFR': mfr_avg,
         'MAR': mar_avg
     }
-
-# # Usage example
-# json_file = 'path_to_your_json_file.json'  # Replace with your JSON file path
-# metrics = calculate_metrics(json_file)
-# print(metrics)
 
 # Usage example
 json_file = f'crossvalidation/{project_name}/{project_name}_merged_data.json'  # Replace with your JSON file path
