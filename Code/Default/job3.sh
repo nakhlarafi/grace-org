9c82cb0c472c8db1cd0fb4846bc6d1a66bf87d44
Nakhla Rafi <nakhla054@gmail.com>
2024-01-24 22:22:32 -0500

    f

--- Code/Default/job3.sh
+++ Code/Default/job3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #SBATCH -J Test-GPUs --mem=100M --gpus=4 -w virya4
 source /etc/profile.d/modules.sh 
-module load anaconda/3.2022.10
+module load anaconda/3.2023.10
 python runtotal.py Collections
