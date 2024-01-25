13afd8adf4fcbe290f5090c61e5b516699cb0f88
Nakhla Rafi <nakhla054@gmail.com>
2024-01-25 17:22:22 -0500

    f

--- Code/Default/job3.sh
+++ Code/Default/job3.sh
@@ -2,14 +2,15 @@
 #SBATCH -J Test-GPUs --mem=100M --gpus=4 -w virya4
 source /etc/profile.d/modules.sh 
 module load anaconda/3.2023.03
-python runtotal.py Cli
-python runtotal.py Codec
-python runtotal.py Compress
-python runtotal.py Csv
-python runtotal.py Gson
-python runtotal.py JacksonCore
-python runtotal.py JacksonXml
-python runtotal.py Jsoup
-python runtotal.py Math
-python runtotal.py Mockito
-python runtotal.py Time
+# python runtotal.py Cli
+# python runtotal.py Codec
+# python runtotal.py Compress
+# python runtotal.py Csv
+# python runtotal.py Gson
+# python runtotal.py JacksonCore
+# python runtotal.py JacksonXml
+# python runtotal.py Jsoup
+# python runtotal.py Math
+# python runtotal.py Mockito
+# python runtotal.py Time
+python runtotal.py Collections
