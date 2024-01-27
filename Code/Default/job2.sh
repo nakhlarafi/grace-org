e5a50b846cbf3a99b377ad8532fe64c9ec217db6
Nakhla Rafi <nakhla054@gmail.com>
2024-01-27 17:49:16 -0500

    change

--- Code/Default/job2.sh
+++ Code/Default/job2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J Test-GPUs --mem=100M --gpus=4 -w virya4
+#SBATCH -J Test-GPUs --mem=100M --gpus=4 -w virya3
 
 source /etc/profile.d/modules.sh
 module load anaconda/3.2023.03
