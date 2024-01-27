71917fe4957b9f9fd2c8fe7f9b69bb400c491434
Nakhla Rafi <nakhla054@gmail.com>
2024-01-26 21:01:55 -0500

    change in code

--- Code/Default/job5.sh
+++ Code/Default/job5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Array of MIDs
-MIDs=("Cli" "Codec" "Collections" "Compress" "Csv" "Gson" "JacksonCore" "JacksonXml" "Jsoup" "Math" "Mockito")
+MIDs=("Cli" "Codec" "Collections" "Compress" "Csv" "Gson" "JacksonCore" "JacksonXml" "Jsoup" "Math" "Mockito" "Time")
 
 # List of project names
 projects=("Cli" "Codec" "Collections" "Compress" "Csv" "Gson" "JacksonCore" "JacksonXml" "Jsoup" "Lang" "Math" "Mockito" "Time")
