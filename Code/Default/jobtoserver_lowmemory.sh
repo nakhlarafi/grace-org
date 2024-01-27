d992c123abb744ea3e9bf6f00f9adb4bdf7761e1
Nakhla Rafi <nakhla054@gmail.com>
2024-01-26 21:14:19 -0500

    change in job

--- Code/Default/jobtoserver_lowmemory.sh
+++ Code/Default/jobtoserver_lowmemory.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#SBATCH -J depgraph-test
-#SBATCH --mem=100M
-#SBATCH -o depgraph-test.out
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=nakhla054@gmail.com 
+#SBATCH --mem=100M -n1 --gpus=1 -J depgraphjob
 
 source /etc/profile.d/modules.sh
 module load anaconda/3.2023.03
