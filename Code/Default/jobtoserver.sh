3d82a2bbebce0fcdbf5eeaf64f26e32408d31bcf
Nakhla Rafi <nakhla054@gmail.com>
2024-01-26 23:06:51 -0500

    change

--- Code/Default/jobtoserver.sh
+++ Code/Default/jobtoserver.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#SBATCH -J Test-GPUs --mem=100M --gpus=4 -w virya4
+#SBATCH -J Test-GPUs
+#SBATCH --mem=10GB 
+#SBATCH -o _%x%J.out
+#SBATCH --gpus=1
+#SBATCH --mail-type=BEGIN,END,FAIL --mail-user=nakhla054@gmail.com
+
 source /etc/profile.d/modules.sh 
 module load anaconda/3.2023.03
-bash job.sh
+bash job5.sh
