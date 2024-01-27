b61d3119c1e5eb7434fa968111fb3c77947ab9d0
Nakhla Rafi <nakhla054@gmail.com>
2024-01-26 20:33:26 -0500

    change in job 1

--- Code/Default/job.sh
+++ Code/Default/job.sh
@@ -3,6 +3,20 @@
 #SBATCH --mail-type=BEGIN,END 
 #SBATCH --mail-user=nakhla054@gmail.com 
 
-source /etc/profile.d/modules.sh
-module load anaconda/3.2023.03
-bash job5.sh
+# Array of MIDs
+MIDs=("Cli" "Codec" "Collections" "Compress" "Csv" "Gson" "JacksonCore" "JacksonXml" "Jsoup" "Math" "Mockito")
+
+# List of project names
+projects=("Cli" "Codec" "Collections" "Compress" "Csv" "Gson" "JacksonCore" "JacksonXml" "Jsoup" "Lang" "Math" "Mockito" "Time")
+
+# Loop through each MID
+for MID in "${MIDs[@]}"; do
+    echo "Processing MID: ${MID}"
+
+    # Loop through each project for the current MID
+    for project in "${projects[@]}"; do
+        echo "Running tests for project ${project} with MID ${MID}"
+        python run_test.py ${project} ${MID}
+        python top_k.py ${project} ${MID}
+    done
+done
